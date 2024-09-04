7b33558ca7cd4d79da7696c08b89512181a78a96
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-09-04 18:31:45 +0530

    nested layout and link

--- next.js/app/datafetch/page.tsx
+++ next.js/app/datafetch/page.tsx
@@ -1,19 +1,36 @@
-import React from 'react'
+// import React from 'react'
 
-const fecth = async () => {
-    type User = {
-        name: string, id: number, username: string, email: string, phone: string
+// const fecth = async () => {
+//     type User = {
+//         name: string, id: number, username: string, email: string, phone: string
 
-    }
+//     }
 
 
-    const response = await fetch('https://jsonplaceholder.typicode.com/users')
-    let user = response.json()
-    console.log(user);
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         if (!response.ok) {
+//             return { messge: 'error' }
+//         }
+//         let users: User[] = await response.json()
+//         return (
+//             <div>
+//                 <p>data fectch</p>
+    
+//          {users.map((user)=>{
+//             <p key={user.id}>{user.name}</p>
+//          })}
+    
+//             </div>
+    
+//         )
+    
 
-    return (
-        <p>data fetch</p>
-    )
-}
+//     } catch (error) {
+// console.error('erorrrrrrrrrrrr');
 
-export default fecth
+//     }
+   
+// }
+
+// export default fecth
